f6e0941e85e5bedbd3835d8e161f4c20043e2824
horlab303 <iamcull@hotmail.com>
2022-07-04 09:18:52 -0400

    apagando arq do client para poder fazer o merge sem conflitos

--- client/src/auth/login/verificarLogin.js
+++
@@ -1,9 +0,0 @@
-export const verificaLogin = {
-  verificarEmail: (emailEntrada) => {
-    return emailEntrada === "anderson.scr@outlook.com"
-  },
-  verificarSenha: (senhaEntrada) => {
-    return senhaEntrada === "123"
-  }
-
-}
