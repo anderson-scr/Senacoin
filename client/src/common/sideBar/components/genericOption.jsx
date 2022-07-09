132d7968070800507d1a95b001f7b76f094160aa
Anderson <anderson.scr@outlook.com>
2022-07-09 18:24:56 -0400

    Arrumando Navlink da barra lateral

--- client/src/common/sideBar/components/genericOption.jsx
+++ client/src/common/sideBar/components/genericOption.jsx
@@ -5,19 +5,19 @@ const GenericOption = ({ icon, dataToolTip, path, isOpen}) => {
   const containerText = useRef();
 
   return (
-    <li className={
-      isOpen? '' : 'toolTipHover'
-    } data-datatooltip={dataToolTip}>
-      <NavLink to={path} >
-        {icon}
-      </NavLink>
-      <div className='textMenuOption' ref={containerText} style={isOpen? 
-        {opacity: '1', transitionDelay: '.3s', display: 'inline'}
-          : 
-        {opacity: '0', transitionDelay: '0s', display: 'none'}} >
-        {dataToolTip}
-      </div>
-    </li>
+    <NavLink to={path} >
+      <li className={
+        isOpen? '' : 'toolTipHover'
+      } data-datatooltip={dataToolTip}>
+          {icon}
+          <div className='textMenuOption' ref={containerText} style={isOpen? 
+            {opacity: '1', transitionDelay: '.3s', display: 'inline'}
+            : 
+            {opacity: '0', transitionDelay: '0s', display: 'none'}} >
+            {dataToolTip}
+          </div>
+      </li>
+    </NavLink>
   )
 }
 
