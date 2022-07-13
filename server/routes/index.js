7daebd635568a2671872ac4f050dd3ba3c9be234
horlab303 <iamcull@hotmail.com>
2022-07-13 11:19:50 -0400

    adicionado rotas para unidade e perfil

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,9 @@ router.use('/api/v1/colaborador', require('./colaborador'));
 
 router.use('/api/v1/item', require('./item'));
 
+router.use('/api/v1/unidade', require('./unidade'));
+
+router.use('/api/v1/perfil', require('./perfil'));
+
 
 module.exports = router;
