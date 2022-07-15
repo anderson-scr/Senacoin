5334c0128d5ff2bcdfd14f9dabdc34132da2b069
Mateus Borges <mateusgaspio@gmail.com>
2022-07-15 11:27:27 -0400

    Adicionado o primeiro Select

--- client/src/pages/relatorios/relatorios.jsx
+++ client/src/pages/relatorios/relatorios.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useState } from "react";
 
 const Relatorios = () => {
   return (
-    <div>Relatorios</div>
+    <>
+      <form>
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Selecione seu relatôrio</option>
+          <option value="1">Teste Um</option>
+          <option value="2">Teste Dois</option>
+          <option value="3">Teste Três</option>
+        </select>
+      </form>
+    </>
   )
+
 }
 
 export default Relatorios
