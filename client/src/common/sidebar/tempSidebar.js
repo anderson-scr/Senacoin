205868b5b8ef8575ee143e652ef701096af519fb
Mateus Borges <mateusgaspio@gmail.com>
2022-07-15 09:28:04 -0400

    Excluída minha sidebar

--- client/src/common/sidebar/tempSidebar.js
+++
@@ -1,9 +0,0 @@
-const btnMenu = document.querySelector('#btnMenu')
-const sidebar = document.querySelector('.sidebar')
-
-function teste() {
-    sidebar.classList.toggle("active")
-    // alert('salve')
-}
-
-btnMenu.addEventListener('click', teste)
