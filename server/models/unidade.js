b7effc523402b6731ec0253ff0cfbcb23c589c1d
junioroff <odairfernandesjunior@gmail.com>
2022-07-19 08:13:41 -0400

    .

--- server/models/unidade.js
+++ server/models/unidade.js
@@ -7,6 +7,6 @@ exports.UnidadeSchema = new mongoose.Schema({
 	logradouro: {type: String, default: null},
 	numero: {type: Number, default: null},
 	telefone: {type: String, default: null},
-	resposavel: {type: String, required: true},
+	responsavel: {type: String, required: true},
 	id_status: {type: mongoose.Types.ObjectId, ref: "Status", required: true} //.populate("id_status")
 }, { versionKey: false });
