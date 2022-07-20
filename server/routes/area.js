752980569e613ca06bdc80ca4f0bf0eb9489b84e
diegotfranco <diegotfranco@gmail.com>
2022-07-20 01:16:32 -0400

    finalizado a funcao authUserRole para verificar permissao na rota

--- server/routes/area.js
+++ server/routes/area.js
@@ -4,19 +4,19 @@ const utils = require('../libs/utils');
 
 
 // add a new area
-router.post('/add', utils.authMiddleware, controller.new);
+router.post('/add', utils.authUserMiddleware, utils.authRoleMiddleware("cad_areas"), controller.new);
 // add a new area list
-router.post('/populate', utils.authMiddleware, controller.newList);
+router.post('/populate', utils.authUserMiddleware, utils.authRoleMiddleware("cad_areas"), controller.newList);
 // list all areas
-router.get('/all', utils.authMiddleware, controller.listAll);
+router.get('/all', utils.authUserMiddleware, utils.authRoleMiddleware("cad_areas"), controller.listAll);
 // list all active areas
-router.get('/active', utils.authMiddleware, controller.listActive);
+router.get('/active', utils.authUserMiddleware, utils.authRoleMiddleware("cad_areas"), controller.listActive);
 // list single area
-router.get('/:id', utils.authMiddleware, controller.listOne);
+router.get('/:id', utils.authUserMiddleware, utils.authRoleMiddleware("cad_areas"), controller.listOne);
 // edit a area
-router.patch('/:id', utils.authMiddleware, controller.edit);
+router.patch('/:id', utils.authUserMiddleware, utils.authRoleMiddleware("cad_areas"), controller.edit);
 // delete a area
-router.delete('/:id', utils.authMiddleware, controller.delete);
+router.delete('/:id', utils.authUserMiddleware, utils.authRoleMiddleware("cad_areas"), controller.delete);
 
 
 module.exports = router;
--- server/routes/qrcode.js
+++ server/routes/qrcode.js
@@ -4,19 +4,19 @@ const utils = require('../libs/utils');
 
 
 // ad a new qr code
-router.post('/add', utils.authMiddleware, controller.new);
+router.post('/add', utils.authUserMiddleware, utils.authRoleMiddleware("cad_qrcodes"), controller.new);
 // add a new qr code list
-router.post('/populate', utils.authMiddleware, controller.newList);
+router.post('/populate', utils.authUserMiddleware, utils.authRoleMiddleware("cad_qrcodes"), controller.newList);
 // list all qr codes
-router.get('/all', utils.authMiddleware, controller.listAll);
+router.get('/all', utils.authUserMiddleware, utils.authRoleMiddleware("ger_qrcodes"), controller.listAll);
 // list all active qr codes
-router.get('/active', utils.authMiddleware, controller.listActive);
+router.get('/active', utils.authUserMiddleware, utils.authRoleMiddleware("ger_qrcodes"), controller.listActive);
 // list single qr code
-router.get('/:id', utils.authMiddleware, controller.listOne);
+router.get('/:id', utils.authUserMiddleware, utils.authRoleMiddleware("ger_qrcodes"), controller.listOne);
 // edit a qr code
-router.patch('/:id', utils.authMiddleware, controller.edit);
+router.patch('/:id', utils.authUserMiddleware, utils.authRoleMiddleware("ger_qrcodes"), controller.edit);
 // delete a qr code
-router.delete('/:id', utils.authMiddleware, controller.delete);
+router.delete('/:id', utils.authUserMiddleware, utils.authRoleMiddleware("cad_qrcodes"), controller.delete);
 
 
 module.exports = router;
